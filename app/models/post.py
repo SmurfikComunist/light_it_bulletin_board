55051a14d492df9d3a1b49c67097d9a0ea000b11
SmurfikComunist <test.webserver.http@gmail.com>
2021-08-15 18:12:48 +0300

    feat: add sqlalchemy Post model

---
+++ app/models/post.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import (
+    Column,
+    Text,
+    String,
+)
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+
+class Post(Base):
+    author = Column(String(length=100))
+    title = Column(String(length=100))
+    text = Column(Text(length=1000))
+    comments = relationship("Comment", back_populates="post")
